b47e585f691665e4f24e4aff7a519424c7cff951
Jaeya <87467649+Jaeya@users.noreply.github.com>
2021-08-23 18:10:19 +0900

    Add files via upload

--- Ex_String.h
+++ Ex_String.h
@@ -1,34 +1,35 @@
 #pragma once
 
-
-class Mystring
+class MyString
 {
 public:
-	Mystring(); // 기본 생성자
-	Mystring(const char* pszParam); // 변환 생성자
-	Mystring(const Mystring& pszParam); // 변환 생성자
+    MyString();//기본 생성자
+    MyString(const char* pszParam);//변환 생성자
+    MyString(const MyString& pszParam);//복사 생성자
 
-	~Mystring();
+    ~MyString();
 
-	//연산자 오버로딩
-	// 
-	// left = right
-	Mystring& operator=(const Mystring& rhs);
+    //연산자 오버로딩
+    // left = right;
+    // left operator=(right);
+    MyString& operator=(const MyString& right);
 
-	//정적멤버 변수(=클래스의 멤버변수)
-	
-	static int m_nCount;// 현재까지 생성된 인스턴스의 갯수 관리용
+    //정적멤버 변수(=클래스의 멤버변수)
+    static int m_nCount;//현재까지 생성된 인스턴스의 갯수 관리용
 
 private:
-	
-	char* m_pszData; // 동적메모리(4b)--> malloc/free
-	int m_nLength; // 문자열의 길이
+    char* m_pszData;//동적메모리(4B)-->malloc/free
+    int m_nLength;//문자열의 길이
 
 public:
-	int SetString(const char* pszParam);
-	const char* Getstring() const;
+    int SetString(const char* pszParam);
+    const char* GetString() const;
 private:
-	void Release(); //동적 메모리 해제함수
-	
-		
+    void Release();//동적 메모리 해제함수
+};
+
+class FunnyString : public MyString
+{
+public:
+    int SetString(const char* pszParam);
 };
