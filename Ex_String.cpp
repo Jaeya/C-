79cce904dcbb6457d58588e0cc5099116c3d6ae6
Jaeya <87467649+Jaeya@users.noreply.github.com>
2021-08-23 18:10:07 +0900

    Add files via upload

--- Ex_String.cpp
+++ Ex_String.cpp
@@ -1,85 +1,94 @@
 #include <iostream>
-
 using namespace std;
 
 #include "Ex_String.h"
 
-int Mystring::m_nCount = 0;
+int MyString::m_nCount = 0;//정적멤버는 "정의"를 해줘야 함
+
+
 //기본 생성자
-Mystring::Mystring()
+MyString::MyString()
+    : m_nLength(0), m_pszData(NULL)
 {
-	m_nCount++;
+    m_nCount++;
 
-	m_nLength = 0;
-	m_pszData = NULL;
-	cout << "Mystring의 기본생성자" << endl;
-
+    //m_nLength = 0;
+    //m_pszData = NULL;   
+    cout << "MyString의 기본생성자" << endl;
 }
 
 //변환 생성자
-Mystring::Mystring(const char* pszParam) 
-	:m_nLength(0),m_pszData(NULL)
+MyString::MyString(const char* pszParam)
+    : MyString()
 {
-	
-	/*m_nLength = 0;
-	m_pszData = NULL;*/
-	cout << "Mystring의 변환생성자" << endl;
-	SetString(pszParam);
+    cout << "MyString의 변환생성자" << endl;
+    SetString(pszParam);
 }
 
 //복사 생성자
-Mystring::Mystring(const Mystring& pszParam) 
-	:m_nLength(0), m_pszData(NULL)
+MyString::MyString(const MyString& pszParam)
+    : MyString()
 {
-	
-	cout << "Mystring의 복사생성자" << endl;
-	SetString(pszParam.Getstring());
+    cout << "MyString의 복사생성자" << endl;
+    SetString(pszParam.GetString());
 }
 
-
-Mystring::~Mystring()
+MyString::~MyString()
 {
-	m_nCount--;
-	Release();
+    m_nCount--;
+    this->Release();
 }
 
-Mystring& Mystring:: operator=(const Mystring& right)
+MyString& MyString::operator=(const MyString& right)
 {
-	if (this != &right)
-		this->SetString(right.Getstring());
+    // 자기 자신에 대한 대입이면 아무것도 하지 않는다.
+    if (this != &right)
+        this->SetString(right.GetString());
 
-	return *this;
+    return *this;
 }
 
-int Mystring::SetString(const char* pszParam)
+int MyString::SetString(const char* pszParam)
 {
+    Release();//이전 데이터 삭제
 
-	Release();// 이전 데이터 삭제
+    if (pszParam == NULL)
+        return 0;
 
-	if (pszParam == NULL)
-		return 0;
+    int nLength = strlen(pszParam);
+    if (nLength == 0)
+        return 0;
 
-	int nLength = strlen(pszParam);
-	if (nLength == 0)
-		return 0;
+    m_pszData = new char[nLength + 1];
+    strcpy_s(m_pszData, sizeof(char) * (nLength + 1), pszParam);
 
-	m_pszData = new char[nLength + 1];
-	strcpy_s(m_pszData,sizeof(char)*(nLength +1), pszParam);
-	m_nLength = nLength;
+    m_nLength = nLength;
 
-	return 0;
-	
-	
+    return 0;
 }
-const char* Mystring::Getstring() const
-{
-	return m_pszData;
-}
-void Mystring::Release()
-{
-	if (m_pszData != NULL)
-		delete[] m_pszData;
 
-	m_pszData = NULL;
-	m_nLength = NULL;
+const char* MyString::GetString() const
+{
+    return m_pszData;
+}
+
+void MyString::Release()//동적 메모리 해제함수
+{
+    if (m_pszData != NULL)
+        delete[] m_pszData;
+
+    m_pszData = NULL;
+    m_nLength = 0;
+}
+
+int FunnyString::SetString(const char* pszParam)
+{
+    int nResult;
+
+    if (strcmp(pszParam, "바보") == 0)
+        nResult = MyString::SetString("천재천재");
+    else
+        nResult = MyString::SetString(pszParam);
+
+    return nResult;
 }
